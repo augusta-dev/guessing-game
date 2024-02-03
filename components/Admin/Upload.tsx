1560e2a2c69a2aca02f727dc6e316a1666c5a907
augustan-dev <augustan.dev@gmail.com>
2024-02-03 22:42:57 +0300

    update

--- components/Admin/Upload.tsx
+++ components/Admin/Upload.tsx
@@ -2,21 +2,22 @@
 import React, { useContext } from "react";
 import { UploadButton } from "@uploadthing/react";
 import { SeriesContext } from "../Contexts/SeriesContext";
+import { OurFileRouter } from "@/app/api/uploadthing/core";
 
 const Upload: React.FC = () => {
 	const seriesCtx = useContext(SeriesContext);
 
 	return (
 		<div className="w-full p-2 border-2 border-cyan rounded-lg mt-2 text-cyan">
-			<UploadButton
+			<UploadButton<OurFileRouter>
 				className="text-cyan"
 				endpoint="imageUploader"
-				onClientUploadComplete={(response: Response) => {
+				onClientUploadComplete={(response: Response):void => {
 					response.map((res) => {
 						seriesCtx.setImages(res.url);
 					});
 				}}
-				onUploadError={(error: Error) => {
+				onUploadError={(error: Error):void => {
 					console.log("Error: ", error);
 				}}
 			></UploadButton>

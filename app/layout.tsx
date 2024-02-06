8955231022cb5bc85d1b521bf06fd0f2c27af374
augustan-dev <augustan.dev@gmail.com>
2024-02-06 21:31:09 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,32 @@
 import type { Metadata } from "next";
 import "tailwindcss/tailwind.css"; //never forget this
 import localFont from "next/font/local";
+import { Amaranth, El_Messiri, Poppins, Quattrocento_Sans } from 'next/font/google'
 
 export const metadata: Metadata = {
 	title: "Guess Who: TV Show Edition",
 	description:
 		"Get ready for a hilarious challenge with your friends, testing your knowledge of iconic sitcom characters from Friends and The Office. One player acts as the host, choosing a character from either show. They can describe the character`&apos`s personality, quirks, or memorable moments without revealing their name. The other players have a limited number of guesses (try 3 attempts for a fast-paced game, or 5 for a more relaxed one). Each guess can be based on the host`&apos`s clues or anything they remember about the characters.",
 };
-export const amaranth = localFont({
-	src: [
-		{
-			path: "../assets/Font/Amaranth-Regular.ttf",
-		},
-	],
+
+ const amaranth = Amaranth({
+	subsets: ['latin'],
+	weight: ["400" , "700"],
 	variable: "--font-amaranth",
 });
-export const elMessiri = localFont({
-	src: [
-		{
-			path: "../assets/Font/ElMessiri-VariableFont_wght.ttf",
-		},
-	],
+export const elMessiri = El_Messiri({
+	subsets: ['latin'],
+	weight: ["400" , '500', '600', "700"],
 	variable: "--font-el-messiri",
 });
-export const poppins = localFont({
-	src: [
-		{
-			path: "../assets/Font/Poppins-Regular.ttf",
-		},
-	],
+export const poppins = Poppins({
+	subsets: ['latin'],
+	weight: ["400" , '200', "700"],
 	variable: "--font-poppins",
 });
-export const quattrocentoSans = localFont({
-	src: [
-		{
-			path: "../assets/Font/QuattrocentoSans-Regular.ttf",
-		},
-	],
+export const quattrocentoSans = Quattrocento_Sans({
+	subsets: ['latin'],
+	weight: ["400" , "700"],
 	variable: "--font-quattrocento-sans",
 });
 

7b42d1ad96dc13d9c50885e049e8fa60e466d462
augustan-dev <augustan.dev@gmail.com>
2024-02-06 21:54:24 +0300

    update

--- libs/mongodb.ts
+++ libs/mongodb.ts
@@ -1,11 +1,13 @@
-import mongoose from 'mongoose';
-async function connectMongo () {
+import mongoose from "mongoose";
+let passcode = process.env.MONGODB_URI ? process.env.MONGODB_URI : "";
+
+async function connectMongo() {
 	try {
-		await mongoose.connect(process.env.MONGODB_URI);
+		await mongoose.connect(passcode);
 		console.log("Connected to mongodb successful");
 	} catch (e) {
 		// throw new Error("Error connecting to Mongo");
-        console.log("Error connecting to Mongo")
+		console.log("Error connecting to Mongo");
 	}
-};
+}
 export default connectMongo;

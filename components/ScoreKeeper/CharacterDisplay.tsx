ae314f16b07025d972f8d7ae2cf2b654f870cb5b
augustan-dev <augustan.dev@gmail.com>
2024-02-11 00:15:24 +0300

    update

--- components/ScoreKeeper/CharacterDisplay.tsx
+++ components/ScoreKeeper/CharacterDisplay.tsx
@@ -60,7 +60,7 @@ export default function CharacterDisplay() {
 	return (
 		<div>
 			<CharacterImage number={propnumber}></CharacterImage>
-			<div className="flex justify-between -mt-1 px-4">
+			<div className="flex justify-between -mt-1 px-8">
 				<button onClick={getPrevChar}>
 					{" "}
 					<Arrow
